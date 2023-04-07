d1f02401c057c60d108ed28ecb400ea42ea4151d
Gafurov-01 <amirtimur987@gmail.com>
2023-04-07 12:48:54 +0300

    [login and register have been done] i created auth service for this

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,56 @@
-import { Injectable } from '@nestjs/common'
+import { ForbiddenException, Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { User } from '@prisma/client'
+import { compare } from 'bcrypt'
+import { UserService } from 'src/user/user.service'
+import { AuthDto } from './dtos/auth.dto'
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  public async register(authDto: AuthDto) {
+    const user = await this.userService.getByName(authDto.name)
+
+    if (user) {
+      throw new ForbiddenException('Это имя уже занято')
+    } else {
+      const newUser = await this.userService.create(authDto)
+
+      return {
+        ...newUser,
+        accessToken: await this.issueToken(newUser),
+      }
+    }
+  }
+
+  public async login(authDto: AuthDto) {
+    const user = await this.userService.getByName(authDto.name)
+
+    if (!user) {
+      throw new ForbiddenException('Пользователь с таким именем не существует')
+    } else {
+      await this.checkUserPassword(authDto, user)
+
+      return {
+        ...user,
+        accessToken: await this.issueToken(user),
+      }
+    }
+  }
+
+  private async issueToken(user: User) {
+    return await this.jwtService.signAsync({ sub: user.id })
+  }
+
+  private async checkUserPassword(authDto: AuthDto, user: User) {
+    const isPasswordCorrect = await compare(authDto.password, user.password)
+
+    if (!isPasswordCorrect) {
+      throw new ForbiddenException('Неправильный пароль')
+    }
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@nestjs/common'
+import { genSalt, hash } from 'bcrypt'
+import { AuthDto } from 'src/auth/dtos/auth.dto'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
 export class UserService {
+  private readonly SALT: number = 5
+
   constructor(private readonly prisma: PrismaService) {}
+
+  public async create({ name, password }: AuthDto) {
+    password = await hash(password, await genSalt(this.SALT))
+    return await this.prisma.user.create({
+      data: {
+        name,
+        password,
+      },
+    })
+  }
+
+  public async getByName(name: string) {
+    return await this.prisma.user.findUnique({ where: { name } })
+  }
 
   public async getById(id: string) {
     return await this.prisma.user.findUnique({ where: { id } })
